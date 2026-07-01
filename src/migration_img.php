d8c9775a89485297307c57ce57429ca374e27180
noamormentyn <noamormentyn@gmail.com>
2026-07-01 22:35:28 +0200

    résolution et complétion de migration img

--- src/migration_img.php
+++ src/migration_img.php
@@ -30,12 +30,29 @@ foreach ($resultUser as $img) {
     if (!is_null($img["avatar"])) {
         $urlAvatar = $paths['users']['avatar'] . $img["avatar"];    ///['tmp_name' => $cheminLocal, 'name' => $nomFichier, 'type' => mime_content_type($cheminLocal)]
         file_exists($urlAvatar);
-        $s3->upload(['tmp_name' => $urlAvatar,'name' => substr($img["avatar"], 0, -4), 'type' => mime_content_type($urlAvatar)]);
+        $s3->upload(['tmp_name' => $urlAvatar, 'name' => substr($img["avatar"], 0, -4), 'type' => mime_content_type($urlAvatar)]);
+        $userRepo->updateAvatar($img["id"], $urlAvatar);
     }
-    if (!is_null($img["banner"]))
+    if (!is_null($img["banner"])) {
         $urlbanner = $paths['users']['banner'] . $img["banner"];
         file_exists($urlbanner);
-        $s3->upload(['tmp_name' => $urlbanner,'name' => substr($img["banner"], 0, -4), 'type' => mime_content_type($urlbanner)]);
+        $s3->upload(['tmp_name' => $urlbanner, 'name' => substr($img["banner"], 0, -4), 'type' => mime_content_type($urlbanner)]);
+        $userRepo->updateAvatar($img["id"], $urlbanner);
+    }
+}
+foreach ($resultUser as $img) {
+    if (!is_null($img["icon"])) {
+        $urlAvatar = $paths['servers']['icon'] . $img["icon"];    ///['tmp_name' => $cheminLocal, 'name' => $nomFichier, 'type' => mime_content_type($cheminLocal)]
+        file_exists($urlAvatar);
+        $s3->upload(['tmp_name' => $urlAvatar, 'name' => substr($img["icon"], 0, -4), 'type' => mime_content_type($urlAvatar)]);
+        $userRepo->updateAvatar($img["id"], $urlAvatar);
+    }
+    if (!is_null($img["banner"])) {
+        $urlbanner = $paths['servers']['banner'] . $img["banner"];
+        file_exists($urlbanner);
+        $s3->upload(['tmp_name' => $urlbanner, 'name' => substr($img["banner"], 0, -4), 'type' => mime_content_type($urlbanner)]);
+        $userRepo->updateAvatar($img["id"], $urlbanner);
+    }
 }
 
 ?>
